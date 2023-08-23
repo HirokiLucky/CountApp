82a52659e470a461ae923412a6a5d3ee503e16c2
hiroki <nagadon0424@icloud.com>
2023-08-23 23:40:09 +0900

    finish count up

--- app/src/main/java/com/example/count/MainActivity.java
+++ app/src/main/java/com/example/count/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.count;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) this.findViewById(R.id.count_button);
+        TextView textView = (TextView) this.findViewById(R.id.count_text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "ボタンが押されました", Snackbar.LENGTH_SHORT).show();
+                count++;
+                textView.setText((String.valueOf(count)));
+            }
+        });
     }
 }
